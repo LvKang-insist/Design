47fdb8087c3d5449b04a411094fd051f09d9957c
lv_345 <lv345_y@163.com>
2019-04-04 17:39:25 +0800

    添加卡片式布局 cardview

---
+++ app/src/main/java/cn/lvkang/com/actionbar/FruitAdapter.java
@@ -0,0 +1,58 @@
+package cn.lvkang.com.actionbar;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<Fruit> mFruitList;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Fruit fruit = mFruitList.get(i);
+        viewHolder.fruitName.setText(fruit.getName());
+        Glide.with(mContext).load(fruit.getImageId()).into(viewHolder.fruitImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        private final TextView fruitName;
+        private final ImageView fruitImage;
+        private final CardView cardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            fruitImage = itemView.findViewById(R.id.fruit_image);
+            fruitName = itemView.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList,Context context){
+        mFruitList = fruitList;
+        mContext = context;
+    }
+}
--- app/src/main/java/cn/lvkang/com/actionbar/MainActivity.java
+++ app/src/main/java/cn/lvkang/com/actionbar/MainActivity.java
@@ -9,15 +9,32 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private Fruit[] fruits = {
+            new Fruit("哈哈哈", R.drawable.a1),
+            new Fruit("嘻嘻嘻", R.drawable.a2),
+            new Fruit("呵呵呵", R.drawable.a3),
+            new Fruit("呜呜呜", R.drawable.a4),
+            new Fruit("嘎嘎嘎", R.drawable.a5),
+            new Fruit("哼哼哼", R.drawable.a6),
+            new Fruit("急急急", R.drawable.a7),
+    };
+    private List<Fruit> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         //得到ActionBar的实例
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar!= null){
+        if (actionBar != null) {
             //让导航按钮显示出来
             //导航的按钮就叫做HomeAsUp按钮,默认图标是一个返回的箭头,含义是返回到上一个活动
             //这里对他的样式和作用都做了修改
@@ -54,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //传入当前界面的任意布局就行,然后是内容，最后是时间
-                Snackbar.make(v,"是否删除",Snackbar.LENGTH_LONG)
+                Snackbar.make(v, "是否删除", Snackbar.LENGTH_LONG)
                         .setAction("确定", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -65,14 +82,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initFruits();
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        //卡片布局管理器，显示两列
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter = new FruitAdapter(list,MainActivity.this);
+        recyclerView.setAdapter(adapter);
     }
+
+    private void initFruits() {
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            int index = random.nextInt(7);
+            list.add(fruits[index]);
+        }
+    }
+
     //加载Menu文件
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //getMenuInflater可以得到MenuInflater的对象
         // 在调用他的inflate方法就可以创建菜单了.
         //inflate接收两个参数，1，传入布局文件  2，将菜单项添加到那个Menu对象中，
-        getMenuInflater().inflate(R.menu.toolbar,menu);
+        getMenuInflater().inflate(R.menu.toolbar, menu);
         //返回true就会让对象显示出来.
         return true;
     }
@@ -80,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
     //处理Menu的点击事件
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 break;
